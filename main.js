4cb5ad38abdb84e1e15603495f06feedfa8d6961
alexcash <alexcash@me.com>
2013-01-30 17:37:16 -0800

    Quality of Life for local dev
    - Include fixture so you don't have to hit API on every page load
    - Include simple python server to avoid issues with XHR while using fixture
    - Remove my username and move it to userFixture. this will eventually be part of the jquery api

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 Array.prototype.last = function() {return this[this.length-1];};
 
-var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=alex-cash&period=3month&api_key=" + APIkey + "&format=json";
+//var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + user + "&period=3month&api_key=" + APIkey + "&format=json";
+var url = "serverFixture.json";
+
 $.getJSON( url, function(data){
 	$(data.topalbums.album).each(function(){
 		var cur = {
-			name: 	this.name,
+			name:	this.name,
 			artist: this.artist.name,
 			played: this.playcount,
-			art: 	this.image[this.image.length-1]["#text"]
-		}
+			art:	this.image[this.image.length-1]["#text"]
+		};
 		console.log(cur);
 	});
 });
