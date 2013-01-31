d52812ec573ca2f645f058697d5023769a75fedc
alexcash <alexcash@me.com>
2013-01-30 18:16:44 -0800

    it puts the albums on the page

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-Array.prototype.last = function() {return this[this.length-1];};
-
 //var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + user + "&period=3month&api_key=" + APIkey + "&format=json";
 var url = "serverFixture.json";
+var albums = [];
+
+function isLoaded () {
+	for (var i = 0; i < albums.length; i++){
+		var markup = $("<div class='album'><img src='" + albums[i].art + "'></div>");
+		$('.albums').append(markup);
+	}
+}
 
 $.getJSON( url, function(data){
 	$(data.topalbums.album).each(function(){
-		var cur = {
+		albums.push ({
 			name:	this.name,
 			artist: this.artist.name,
 			played: this.playcount,
 			art:	this.image[this.image.length-1]["#text"]
-		};
-		console.log(cur);
+		});
 	});
+	isLoaded();
 });
